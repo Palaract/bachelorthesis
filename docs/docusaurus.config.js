46767b329950efdd19c8b1750b871cf694f3c1df
Willy Mroczowski <willy@mroczowski.de>
2024-03-21 20:57:39 +0100

    set new baseurl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,15 +18,16 @@ const config = {
   url: 'https://bachelor.wm-co.de',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/bachelorthesis',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Willy Mroczowski', // Usually your GitHub org/user name.
-  projectName: 'Bachelorthesis', // Usually your repo name.
+  organizationName: 'palaract', // Usually your GitHub org/user name.
+  projectName: 'bachelorthesis', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
